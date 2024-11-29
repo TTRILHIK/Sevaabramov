d0ccd9256d03a52bbdf71f2fdfeb482371a0d0f3
TTRILHIK <sevaabramov2008@gmail.com>
2024-11-29 12:04:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function hell(){ 
+    alert('Hello super isip11!')
+}
+function hell1(){
+    alert('Hello super isip11!!')
+}
+function hell2(){
+    alert('Hello super isip11!!!')  
+}
+function ShowPassword() {
+    var x = document.getElementById("pin");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
